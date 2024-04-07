ede52a7b97fcbe9749850b8e08db9776e5d39979
nuruzzaman <nur-zaman@users.noreply.github.com>
2024-04-07 13:08:24 +0600

    feat: added product creation states

---
+++ store/slices/createProductSlice.ts
@@ -0,0 +1,54 @@
+import { StateCreator } from "zustand";
+
+type Product = {
+  title: string;
+  categories: string[];
+  description: string;
+  priceInfo: {
+    purchased: number;
+    rent: number;
+    rate: string;
+  }
+};
+
+type ProductSlice = {
+    
+    product: Product;
+    setProduct: (data: Product) => void;
+    // setProductTitle: (title: string) => void;
+    // setProductCategories: (categories: string[]) => void;
+    // setProductDescription: (description: string) => void;
+    // setProductPriceInfo: (priceInfo: { purchased: number; rent: number; rate: string; }) => void;
+
+};
+
+const initialState: Product = {
+  title: "",
+  categories: [],
+  description: "",
+  priceInfo: {
+    purchased: 0,
+    rent: 0,
+    rate: "",
+  }
+};
+
+const createProductSlice: StateCreator<ProductSlice> = (set) => ({
+//     product: initialState,
+
+//   setProduct: (data) =>
+//     set((state) => ({ product: { ...state.product, ...data } })),
+
+
+
+    product: initialState,
+    // title: "",
+    setProduct: (data) => set(() => ({ product: data })),
+    // setProductTitle: (title) => set((state) => ({ title: state.title })),
+    // setProductCategories: (categories) => set((state) => ({ product: {...state.product, categories } })),
+    // setProductDescription: (description) => set((state) => ({ product: {...state.product, description } })),
+    // setProductPriceInfo: (priceInfo) => set((state) => ({ product: {...state.product, priceInfo } })),
+});
+
+export default createProductSlice;
+export type { Product, ProductSlice };

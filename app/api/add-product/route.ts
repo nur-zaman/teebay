6bde8afeb81c9b6688886dfe88c4b3c42d5b06b4
nuruzzaman <nur-zaman@users.noreply.github.com>
2024-04-08 09:09:09 +0600

    feat: added get and add product routes

---
+++ app/api/add-product/route.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  try {
+    const { name, description, price, userId, rate, rent, status, categories } =
+      await request.json();
+
+    // Fetch or create categories
+    const categoryIds = await Promise.all(
+      categories.map(async (category) => {
+        const existingCategory = await prisma.category.findUnique({
+          where: { name: category.name },
+        });
+        if (existingCategory) {
+          return existingCategory.id;
+        } else {
+          const newCategory = await prisma.category.create({
+            data: {
+              name: category.name,
+            },
+          });
+          return newCategory.id;
+        }
+      })
+    );
+
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        userId,
+        rate,
+        rent,
+        status,
+        categories: {
+          connect: categoryIds.map((id) => ({ id })),
+        },
+      },
+      include: {
+        categories: true,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({ status: 200, messege: "Added product successfully" }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        status: 500,
+        messege: "Failed to create the product",
+        error: error,
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ app/api/products/route.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    const status = searchParams.get("status");
+
+    const whereClause = {};
+
+    if (userId) {
+      whereClause.userId = userId;
+    }
+
+    if (status) {
+      whereClause.status = status;
+    }
+
+    const products = await prisma.product.findMany({
+      where: whereClause,
+      include: {
+        categories: true,
+      },
+    });
+
+    return new Response(JSON.stringify(products), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        status: 500,
+        messege: "Failed to fetch products",
+        error: error,
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
